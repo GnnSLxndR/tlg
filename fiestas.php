64e6030eefbb7132f5deb2d42823dbe3820b87fb
GnnSLxnR\GnnSAlexander <gennys.carrasquilla@platypus-agency.com>
2016-11-22 21:30:06 -0500

    pagina fiestas y horarios

---
+++ fiestas.php
@@ -0,0 +1,130 @@
+<?php
+/*
+*Template Name: Fiestas
+*/
+?>
+<!-- Archivo de cabecera global de Wordpress -->
+<?php get_header(); ?>
+<!--Fiestas The Little Gym-->
+
+<div class="parties">
+    <img src="<?php bloginfo('template_url'); ?>/static/img/party-img.png" class="img-responsive"> <!--Image of this section-->
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h2 class="text-center">Fiestas de Cumpleaños</h2>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <h1>Dales una razón más para celebrar en</h1>
+            <h2>The Little Gym</h2>
+            <p>Si estás buscando donde celebrar fiestas de cumpleaños para niños en Bogotá, no busques más, aquí encontrarás todo lo que buscas, una fiesta infantil divertida, novedosa y sin preocupaciones.
+
+                Simplemente no hay sinónimos para describir lo increíble que es una Fiesta en Cumpleaños en The Little Gym Bogotá, pero veamos si esto lo resume bien: La revista internacional ‘Parents’ nombró a The Little Gym como el lugar número uno para celebrar cumpleaños de los niños.</p>
+        </div>
+
+        <div class="col-sm-6">
+            <img src="<?php bloginfo('template_url'); ?>/static/img/parent-magazine.png" class="center-block img-responsive">
+        </div>
+    </div>
+</div>
+
+
+<div class="container party-container">
+    <div class="row">
+        <div class="col-md-6 party-text center-block">
+            <h1 class="text-center">¿Cómo son las fiestas en </h1>
+            <h2 class="text-center">The Little Gym?</h2>
+        </div>
+        <div class="col-md-6">
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="<?php bloginfo('template_url'); ?>/static/img/fiesta-gorro.svg">
+                </div>
+                <div class="col-md-8">
+                    <p>
+                        Disfruta de una fiesta divertida, novedosa y sin preocupaciones, con juegos música y actividades.
+                    </p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="<?php bloginfo('template_url'); ?>/static/img/fiesta-candado.svg">
+                </div>
+                <div class="col-md-8">
+                    <p>
+                        Disfruta de una fiesta divertida, novedosa y sin preocupaciones, con juegos música y actividades.
+                    </p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="<?php bloginfo('template_url'); ?>/static/img/fiesta-reloj.svg">
+                </div>
+                <div class="col-md-8">
+                    <p>
+                        Disfruta de una fiesta divertida, novedosa y sin preocupaciones, con juegos música y actividades.
+                    </p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="<?php bloginfo('template_url'); ?>/static/img/fiesta-ojos.svg">
+                </div>
+                <div class="col-md-8">
+                    <p>
+                        Disfruta de una fiesta divertida, novedosa y sin preocupaciones, con juegos música y actividades.
+                    </p>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</div>
+
+
+
+<div class="container">
+    <div class="party-schedule">
+        <h1>Horarios de</h1>
+        <h2>Fiestas de cumpleaños</h2>
+        <p>Clickea en el horario deseado donde quisieras celebrar</p>
+    </div>
+    <div class="panel panel-default panel-primary panel-schedule">
+        <div class="panel-heading">
+            <h2 class="panel-title">fiestas</h2>
+        </div>
+        <div class="panel-body">
+
+            <ul class="text-center list-group">
+                <li class="list-group-item">
+
+                    <div class="classes-schedule">
+                        <p class="panel-schedule-headings">Sábados</p>
+                        <p>3:00pm - 6:00pm</p>
+                    </div>
+                </li>
+                <li class="list-group-item">
+
+                    <p class="panel-schedule-headings">Domingos</p>
+                    <p>3:00pm - 6:00pm</p>
+                </li>
+
+                <li>
+                    <a href="#" class="btn btn-primary btn-lg btn-tlg btn-block">Inscríbete</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- Archivo de pié global de Wordpress -->
+<?php get_footer(); ?>
---
+++ horarios.php
@@ -0,0 +1,291 @@
+<?php
+/*
+*Template Name: Horarios
+*/
+?>
+<!-- Archivo de cabecera global de Wordpress -->
+<?php get_header(); ?>
+
+<!--Horarios The Little Gym-->
+
+<div class="calendar">
+    <img src="<?php bloginfo('template_url'); ?>/static/img/horarios-img.png" class="img-responsive"> <!--Image of this section-->
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h2 class="text-center">Horarios</h2>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Horarios de Clases </h1>
+            <h2>The Little Gym</h2>
+            <p class="text-uppercase">Bogotá</p>
+            <p>Clickea en la clase deseada e inscríbete</p>
+
+            <!--Panel de Horarios The Little Gym-->
+
+            <div class="panel panel-default panel-primary panel-schedule">
+                <div class="panel-heading">
+                    <h2 class="panel-title">Lunes</h2>
+                </div>
+                <div class="panel-body">
+
+                    <ul class="text-center list-group">
+                        <li class="list-group-item">
+
+                            <div class="classes-schedule">
+                                <p class="panel-schedule-headings">Beasts (19M- 2.5A)</p>
+                                <p>3:00pm - 4:00pm</p>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+
+                            <p class="panel-schedule-headings">Sports Skills(3- 6A)</p>
+                            <p>4:00pm - 5:00pm</p>
+                        </li>
+                        <li class="list-group-item">
+
+                            <p class="panel-schedule-headings">Danza (3- 6A)</p>
+                            <p>5:00pm - 6:00pm</p>
+                        </li>
+                        <li class="list-group-item">
+
+                            <p class="panel-schedule-headings">Karate (4- 6A)</p>
+                            <p>3:00pm - 6:00pm</p>
+                        </li>
+                        <li class="list-group-item">
+
+                            <p class="panel-schedule-headings">Danza y karate (6- 12A)</p>
+                            <p>3:00pm - 6:00pm</p>
+                        </li>
+                        <li>
+                            <a href="#" class="btn btn-primary btn-lg btn-tlg btn-block">Inscríbete</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="panel panel-default panel-primary panel-bordered panel-schedule">
+                <div class="panel-heading">
+                    <h2 class="panel-title">Martes</h2>
+                </div>
+                <div class="panel-body">
+
+                    <ul class="text-center list-group">
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Birds (10- 19M)</p>
+                            <p>10:30pm - 11:30pm</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Super Beasts(2,5- 3A)</p>
+                            <p>4:00pm - 5:00pm</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Funny Bugs(3- 4A)</p>
+                            <p>4:00pm - 5:00pm</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Giggle worms (4- 6A)</p>
+                            <p>5:00pm - 6:00pm</p>
+                        </li>
+                        <li class="">
+                            <a href="#" class="btn btn-primary btn-lg btn-tlg btn-block">Inscríbete</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="panel panel-default panel-primary panel-bordered panel-schedule">
+                <div class="panel-heading">
+                    <h2 class="panel-title">Miércoles</h2>
+                </div>
+                <div class="panel-body">
+
+                    <ul class="text-center list-group">
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Birds (10- 19M)</p>
+                            <p>9:30pm - 10:30pm</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Beasts(19M- 2,5A)</p>
+                            <p>10:30pm - 11:30pm</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Bugs(4- 10M)</p>
+                            <p>11:30pm - 12:30pm</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Beasts (19M- 2,5A)</p>
+                            <p>3:00pm - 4:00pm</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Sports Skills (3- 6A)</p>
+                            <p>4:00pm - 5:00pm</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Flip/Hot Shots (6- 12A)</p>
+                            <p>5:00pm - 6:00pm</p>
+                        </li>
+                        <li class="">
+                            <a href="#" class="btn btn-primary btn-lg btn-tlg btn-block">Inscríbete</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="panel panel-default panel-primary panel-bordered panel-schedule">
+                <div class="panel-heading">
+                    <h2 class="panel-title">Jueves</h2>
+                </div>
+                <div class="panel-body">
+
+                    <ul class="text-center list-group">
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Birds (10- 19M)</p>
+                            <p>9:30pm - 10:30pm</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Beasts(19M- 2,5A)</p>
+                            <p>10:30pm - 11:30pm</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Bugs(4- 10M)</p>
+                            <p>11:30pm - 12:30pm</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Beasts (19M- 2,5A)</p>
+                            <p>3:00pm - 4:00pm</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Sports Skills (3- 6A)</p>
+                            <p>4:00pm - 5:00pm</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Flip/Hot Shots (6- 12A)</p>
+                            <p>5:00pm - 6:00pm</p>
+                        </li>
+                        <li class="">
+                            <a href="#" class="btn btn-primary btn-lg btn-tlg btn-block">Inscríbete</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="panel panel-default panel-primary panel-bordered panel-schedule">
+                <div class="panel-heading">
+                    <h2 class="panel-title">Viernes</h2>
+                </div>
+                <div class="panel-body">
+
+                    <ul class="text-center list-group">
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Birds (10- 19M)</p>
+                            <p>9:30pm - 10:30pm</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Beasts(19M- 2,5A)</p>
+                            <p>10:30pm - 11:30pm</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Bugs(4- 10M)</p>
+                            <p>11:30pm - 12:30pm</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Beasts (19M- 2,5A)</p>
+                            <p>3:00pm - 4:00pm</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Sports Skills (3- 6A)</p>
+                            <p>4:00pm - 5:00pm</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Flip/Hot Shots (6- 12A)</p>
+                            <p>5:00pm - 6:00pm</p>
+                        </li>
+                        <li class="">
+                            <a href="#" class="btn btn-primary btn-lg btn-tlg btn-block">Inscríbete</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="panel panel-default panel-primary panel-bordered panel-schedule">
+                <div class="panel-heading">
+                    <h2 class="panel-title">Sábado</h2>
+                </div>
+                <div class="panel-body">
+
+                    <ul class="text-center list-group">
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Birds (10- 19M)</p>
+                            <p>9:30pm - 10:30pm</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Beasts(19M- 2,5A)</p>
+                            <p>10:30pm - 11:30pm</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Bugs(4- 10M)</p>
+                            <p>11:30pm - 12:30pm</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Beasts (19M- 2,5A)</p>
+                            <p>3:00pm - 4:00pm</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Sports Skills (3- 6A)</p>
+                            <p>4:00pm - 5:00pm</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Flip/Hot Shots (6- 12A)</p>
+                            <p>5:00pm - 6:00pm</p>
+                        </li>
+                        <li class="">
+                            <a href="#" class="btn btn-primary btn-lg btn-tlg btn-block">Inscríbete</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="panel panel-default panel-primary panel-bordered panel-schedule">
+                <div class="panel-heading">
+                    <h2 class="panel-title">Domingo</h2>
+                </div>
+                <div class="panel-body">
+
+                    <ul class="text-center list-group">
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Birds (10- 19M)</p>
+                            <p>9:30pm - 10:30pm</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Beasts(19M- 2,5A)</p>
+                            <p>10:30pm - 11:30pm</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Bugs(4- 10M)</p>
+                            <p>11:30pm - 12:30pm</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Beasts (19M- 2,5A)</p>
+                            <p>3:00pm - 4:00pm</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Sports Skills (3- 6A)</p>
+                            <p>4:00pm - 5:00pm</p>
+                        </li>
+                        <li class="list-group-item">
+                            <p class="panel-schedule-headings">Flip/Hot Shots (6- 12A)</p>
+                            <p>5:00pm - 6:00pm</p>
+                        </li>
+                        <li class="">
+                            <a href="#" class="btn btn-primary btn-lg btn-tlg btn-block">Inscríbete</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+    </div>
+    </div>
+</div>
+
+<!-- Archivo de pié global de Wordpress -->
+<?php get_footer(); ?>
